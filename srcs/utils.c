45748c1be8475cff0304410b6d58bfee432211f0
Timmy FURET <tifuret@e3r4p6.42.fr>
2018-06-01 17:12:57 +0200

    Add fast comment

--- srcs/utils.c
+++ srcs/utils.c
@@ -67,6 +67,10 @@ void	ft_keys(t_fractol *e)
 	mlx_hook(e->win, 6, 0, ft_mouse_julia, e);
 }
 
+/*
+** Function to set the fractal for all the window
+*/
+
 void	ft_set_all_window(t_fractol *e)
 {
 	e->x2 = e->x;
